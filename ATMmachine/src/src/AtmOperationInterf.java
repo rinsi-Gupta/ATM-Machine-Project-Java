5a825bf15aeb5043f8216798765286fd10aa8e7f
rinsi-Gupta <rinsigupta7982@gmail.com>
2023-09-21 14:38:46 +0530

    ATM Machine Project Code

---
+++ ATMmachine/src/src/AtmOperationInterf.java
@@ -0,0 +1,8 @@
+package src;
+
+public interface AtmOperationInterf {
+	public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount );
+    public  void viewMiniStatement();
+}
